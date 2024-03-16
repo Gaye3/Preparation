d2aeaceaf4b96e5671e5f9ac02d66ae3f905fcc1
Gayathri B <gayathri.gaye18@gmail.com>
2024-03-16 15:10:28 +0530

    This file added from newBranch and available in new branch

---
+++ src/main/java/oopsBasics/InheritanceParentClass.java
@@ -0,0 +1,14 @@
+package oopsBasics;
+
+public class InheritanceParentClass {
+	
+	public void start() {
+		System.out.println("Parent class method starting");
+	}
+	
+	
+	public void end() {
+		System.out.println("Parent class method ending");
+	}
+
+}
